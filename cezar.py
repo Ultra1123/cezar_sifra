4b0e153ec1b4db7b79b02826bc1b5e0e426af88f
Dax00 <91143879+Ultra1123@users.noreply.github.com>
2023-05-26 13:09:08 +0200

    Add files via upload

---
+++ cezar.py
@@ -0,0 +1,12 @@
+import string
+
+plain_text = "David Pokorni"
+shift = 3
+
+alphabet = string.ascii_lowercase
+shifted = alphabet[shift:] + alphabet[:shift]
+table = str.maketrans(alphabet, shifted)
+
+encrypted = plain_text.translate(table)
+
+print(encrypted)
